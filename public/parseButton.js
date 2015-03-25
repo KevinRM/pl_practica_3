9edd626f77897422815d243a330f5f3325fb1348
alu0100764666 <alu0100764666@ull.edu.es>
2015-03-25 22:18:41 +0000

    Cambiado a estatico

--- public/parseButton.js
+++ public/parseButton.js
@@ -1,14 +1,8 @@
 $(document).ready(function () {
-    
+
     $("#parse").click(function () {
-      
-        $.get("/parse", { input: document.getElementById("myText").value }, function (data) {
-           // console.log(data);
-            $("#output").html(JSON.stringify(data.answer).replace(/},{/g, "},\n{"));
-                   
-        }, 'json');
+        var data = tokenizar(document.getElementById("myText").value);
+        $("#output").html(JSON.stringify(data).replace(/},{/g, "},\n{"));
+
     });
-
-
-
 });
