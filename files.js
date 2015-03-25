dca5da6133050672d4993677d8d7002f9979d5f0
alu0100764666 <alu0100764666@ull.edu.es>
2015-03-25 18:24:26 +0000

    Arreglado drag and drop

---
+++ files.js
@@ -0,0 +1,61 @@
+
+// Setup the drag and drop listeners.
+//var dropZone = document.getElementById('drop_zone');
+$(document).ready(function () {
+    document.getElementById('drop_zone').addEventListener('dragover', handleDragOver, false);
+    document.getElementById('drop_zone').addEventListener('dragleave', handleMouseOut, false);
+    document.getElementById('drop_zone').addEventListener('drop', handleFileSelect, false);
+    document.getElementById('files').addEventListener('change', CopyMe, false);
+});
+/*
+var input = document.querySelector('#B_clear');
+var textarea = document.querySelector('#myText');
+var res = "";
+*/
+function reset_form_element(x) {
+    x.wrap('<form>').parent('form').trigger('reset');
+    x.unwrap();
+
+}
+function re(x) {
+    document.getElementById('myText').innerHTML = "";
+    reset_form_element($('#files'));
+    x.preventDefault();
+}
+function handleMouseOut(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.target.style.background = "#2E2E2E";
+}
+
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    var file = evt.dataTransfer.files[0];
+    if (file) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            document.getElementById('myText').innerHTML = e.target.result;
+        }
+        var c = reader.readAsText(file);
+    }
+    else { alert("Failed to load file"); }
+    evt.target.style.background = "#2E2E2E";
+}
+function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.target.style.background = "green";
+}
+
+function CopyMe(evt) {
+    var file = evt.target.files[0];
+    if (file) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            myText.innerHTML = e.target.result;
+        }
+        var c = reader.readAsText(file);
+    }
+    else { alert("Failed to load file"); }
+}
